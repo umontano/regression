c94533f14f646c6a2e77a177d5374ce019d4f22a
umontano <u.montano.j@gmail.com>
2025-05-25 03:29:27 -0600

    jitter added to dotplot and commented geom_quasirandom from ggbeeswarm package

--- sig_graph.R
+++ sig_graph.R
@@ -23,11 +23,27 @@
 ## INSTALL LIBRARIES IF NOT INSTALLED, AND LOAD THEM
 packages <- c(
 'ggplot2',
-'gridExtra'
+'gridExtra',
+'ggthemes',
+'tidytext',
+'dplyr',
+'tibble',
+'ggbeeswarm'
 )
 lapply(packages, \(x) if (!require(x, character.only = TRUE)) { install.packages(x)
 	library(x, character.only = TRUE) })
 
+## globasl themes and color scales
+theme_set(theme_economist(
+       base_size = 10,
+       base_family = 'sans',
+       horizontal = TRUE,
+       dkpanel = TRUE))
+options(
+	ggplot2.continuous.colour = scale_colour_stata,
+	ggplot2.continuous.fill = scale_fill_stata,
+	ggplot2.discrete.colour = scale_colour_stata,
+	ggplot2.discrete.fill = scale_fill_stata)
 
 compute_lm <- function(dataset, resp_name, pred_name, significance_threshold = 0.05, r_min_threshold = 0.09)
 {
@@ -41,7 +57,7 @@ compute_lm <- function(dataset, resp_name, pred_name, significance_threshold = 0
 	## CHECK THAT RESPONSE AND PREDICTOR ARE NOT THE SAME
 	if(resp_name == pred_name) return(NULL)
 	## RESPONSE VARIABLE IS NUMERIC AND NOT NA
-	if(!is.numeric(dataset[[resp_name]]) || any(is.na(dataset[[resp_name]])) || any(is.na(dataset[[pred_name]])) ) return(NULL)
+	if(!is.numeric(dataset[[resp_name]])) return(NULL)
 	## GETTING P-VALUE AND COEFFIECIETS
 	formula <- paste(resp_name, '~', pred_name)
 	lmsummary <- dataset |> lm(formula, data = _, na.action = 'na.exclude') |> summary()
@@ -99,9 +115,13 @@ add_significant_jitter_plots <- function(plotee_dataset, significant_analyses_li
 save_grid_plots <- function(plot_list = plot_list, save_graph_to = 'z.png')
 {
 	## CHECK IT IS NOT EMPTY LIST
-	if(length(plot_list) < 1) return(NULL)
-	pgrid <- do.call('grid.arrange', c(plot_list, nrow = round(sqrt(length(plot_list)))))
-	if(any(grepl('\\.(pdf|png|jpg)$', save_graph_to, perl = TRUE))) ggsave(pgrid, file = save_graph_to)
+	nump <- length(plot_list)
+	if(nump < 1) return(NULL)
+	#pgrid <- do.call('grid.arrange', c(plot_list, nrow = round(sqrt(nump))))
+	#pgrid <- marrangeGrob(plot_list, ncol = 4, nrow = ifelse(nump > 3, 4, 1 + floor(nump / 4)))
+	if(nump < 17) pgrid <- do.call('grid.arrange', c(plot_list, nrow = round(sqrt(nump))))
+	else pgrid <- marrangeGrob(plot_list, ncol = 4, nrow = 4)
+	if(length(pgrid) > 0 && any(grepl('\\.(pdf|png|jpg)$', save_graph_to))) ggsave(pgrid, file = save_graph_to)
 	pgrid
 }
 
@@ -124,7 +144,7 @@ add_significant_conditional_jitter_or_dotplot <- function(plotee_dataset, signif
 		## OLD JITTER GEOM FUNCTION
 		p <- ggplot(plotee_dataset, aes(!!as.symbol(predictor_name_or_category), !!as.symbol(response_name))) +
 			geom_jitter(color = '#333366', alpha = opacity) +
-			labs(caption = paste0('p=', each_resp_pred[['min_pvalue']], ', R2=', each_resp_pred[['adjr']]))
+			labs(x = predictor_name_or_category, y = response_name, caption = paste0('p=', each_resp_pred[['min_pvalue']], ', R2=', each_resp_pred[['adjr']]))
 			## ADD ERROR BARS OR REGRESSION LINE
 		if(!is.numeric(predictor_column)) p <- p + stat_summary(fun.data = 'mean_se', geom = 'errorbar', fun.args = list(mult = 1.96), width = 0.4, color = '#663333') +
 			stat_summary(fun = mean, geom = "point", shape = 5, size = 0.4, color = '#663333')
@@ -133,17 +153,28 @@ add_significant_conditional_jitter_or_dotplot <- function(plotee_dataset, signif
 		}
 		else
 		{
-			#print('____NOT_NUM_DOTPLOT_FOR_CHAR/FACT___')
-			#print(predictor_name_or_category)
-	       ## NEW DOTPLOT FUNCTION
-		ggplot(plotee_dataset, aes(!!as.symbol(predictor_name_or_category), !!as.symbol(response_name))) +
-			geom_dotplot(aes(color = !!as.symbol(predictor_name_or_category), fill = !!as.symbol(predictor_name_or_category)), binaxis = 'y', binpositions = 'all', stackdir = 'center', dotsize = 0.6, stackratio = 0.7, alpha = opacity, show.legend = FALSE) +
+		#print('____NOT_NUM_DOTPLOT_FOR_CHAR/FACT___')
+		#print(predictor_name_or_category)
+		plotee_dataset %>%
+		mutate(
+		differencee_response = !!as.symbol(response_name),
+		cat_noordered = !!as.symbol(predictor_name_or_category),
+		cat_reordered = reorder_within(cat_noordered, differencee_response, cat_noordered)) %>%
+#reorder_within(x, by, within, fun = mean, sep = "___", ...)
+#scale_x_reordered(..., labels = reorder_func, sep = deprecated())
+		ggplot(aes(cat_reordered, differencee_response)) +
+			## NEW DOTPLOT FUNCTION
+			geom_dotplot(aes(color = cat_reordered, fill = cat_reordered), binaxis = 'y', binpositions = 'all', stackdir = 'center', dotsize = 0.6, stackratio = 0.95, position = position_jitter(width = 0.00, height = 0.05), alpha = opacity, show.legend = FALSE) +
+			##  USE GGBEESWARM QUASIRANDOM INSTEAD OF DOTPLT
+			#ggbeeswarm::geom_quasirandom(aes(color = cat_reordered, fill = cat_reordered), alpha = opacity, show.legend = FALSE) +
 			geom_boxplot(fill = NA, color = 'grey60', alpha = 0.2, show.legend = FALSE) +
 			## COPYED ROM CATEGORICAL GITHUB
-				stat_summary(fun.data = 'mean_se', geom = 'errorbar', fun.args = list(mult = 1.96), width = 0.4) +
-			    #stat_summary(fun.data = 'mean_se', geom = 'pointrange', alpha = 0.4, color = '#663333') +
+			stat_summary(fun.data = 'mean_se', geom = 'errorbar', fun.args = list(mult = 1.96), width = 0.4) +
+			#stat_summary(fun.data = 'mean_se', geom = 'pointrange', alpha = 0.4, color = '#663333') +
 			stat_summary(fun = mean, geom = "point", shape = 5, size = 0.4) +
-			labs(caption = paste0('p=', each_resp_pred[['min_pvalue']], ', R2=', each_resp_pred[['adjr']]))
+			## REORDER_WITIN SECOND COMPONENT
+			scale_x_reordered() +
+			labs(x = predictor_name_or_category, y = response_name, caption = paste0('p=', each_resp_pred[['min_pvalue']], ', R2=', each_resp_pred[['adjr']]))
 		}
        }, USE.NAMES = TRUE, simplify = FALSE)
 }
